82a34fed25a715cbd29a9906afc5fea3f206bc3f
Ivan Castillo <ivan.castillo@bluumi.net>
2019-09-27 16:01:45 +0200

    up

--- demo_tesseract/src/App.js
+++ demo_tesseract/src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 //var Tesseract = window.Tesseract;
 import { TesseractWorker } from 'tesseract.js';
+import { OcrLineResponseModel } from './OcrLineResponseModel';
+//import scanner from 'scanner-js';
+//import * as scanner from 'scanner-js';
+var scanner = window.scannerjs;
 const worker = new TesseractWorker();
 
 class App extends Component {
@@ -15,13 +20,24 @@ class App extends Component {
     };
   }
 
+  getBase64(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+    });
+  }
+
   handleChange = (event) => {
     if (event.target.files[0]) {
       var uploads = []
       for (var key in event.target.files) {
         if (!event.target.files.hasOwnProperty(key)) continue;
-        let upload = event.target.files[key]
-        uploads.push(URL.createObjectURL(upload))
+        let upload = event.target.files[key];
+        //console.log('FILE: ', upload);
+        uploads.push(upload);
+        //uploads.push(URL.createObjectURL(upload));For Tesseract
       }
       this.setState({
         uploads: uploads
@@ -33,7 +49,8 @@ class App extends Component {
     }
   }
 
-  generateText = () => {
+  /* WITH TESSERACT */
+  generateTesseractText = () => {
     let uploads = this.state.uploads
     for (var i = 0; i < uploads.length; i++) {
       worker.recognize(uploads[i], { lang: 'spa' }).progress(progress => {
@@ -47,7 +64,7 @@ class App extends Component {
         let text = result.text
 
         // Get codes
-        let pattern = /\b\w{10,10}\b/g
+        let pattern = /\b\w{10,10}\b/g;
         let patterns = result.text.match(pattern);
 
         // Update state
@@ -62,7 +79,165 @@ class App extends Component {
         console.log('DOCUMENTS: ', this.state.documents);
       }).catch(err => {
         console.error(err);
-      })
+      });
+    }
+  }
+
+  /* WITH Free OCR API and Base64Image */
+  generatetextFromBase64Image = () => {
+    let uploads = this.state.uploads
+    //console.log('UPLOADS: ', uploads[0]);
+    this.getBase64(uploads[0]).then((base64Image) => {
+
+      let body = new FormData();
+      body.set('apikey', '16e218e05b88957');
+      body.set('language', 'spa');
+      body.set('isOverlayRequired', false);
+      body.set('base64Image', base64Image); // || body.set('file', file) || body.set('url', urlFile);
+      body.set('isTable', true);
+      body.set('isCreateSearchablePdf', false);
+      // body.set('filetype', false); // PNG, JPG, GIF, PDF
+      //body.set('OCREngine', 2);
+
+      let responseLines = new Array();
+
+      axios.post('https://api.ocr.space/parse/image', body).then(res => {
+        const lines = res.data.ParsedResults[0].TextOverlay.Lines;
+        //console.log('RESPONSE LINES: ', res.data.ParsedResults[0].TextOverlay.Lines);
+        lines.forEach((line) => {
+          let ocrResponseModel = new OcrLineResponseModel();
+
+          //console.log('LINE: ', line);
+          ocrResponseModel.lineText = line.LineText;
+          line.Words.forEach((word) => {
+            ocrResponseModel.words.push(word);
+          });
+          responseLines.push(ocrResponseModel);
+        });
+        console.log('OBTAINED BILL: ', responseLines);
+      });
+    });
+  }
+
+  /* WITH Free OCR API and File */
+  generatetextFromFile = () => {
+    let uploads = this.state.uploads
+    console.log('UPLOADS: ', uploads[0]);
+
+    let body = new FormData();
+    body.set('apikey', '16e218e05b88957');
+    body.set('language', 'spa');
+    body.set('isOverlayRequired', false);
+    //body.set('base64Image', base64Image); // || body.set('file', file) || body.set('url', urlFile);
+    body.set('file', uploads[0]);
+    body.set('isTable', true);//Si es pdf tiene que estar a true o no devuelve nada
+    body.set('isCreateSearchablePdf', false);
+    body.set('scale', true);
+    // body.set('filetype', false); // PNG, JPG, GIF, PDF
+
+    let responseLines = new Array();
+
+    axios.post('https://api.ocr.space/parse/image', body).then(res => {
+      const lines = res.data.ParsedResults[0].TextOverlay.Lines;
+      //console.log('RESPONSE LINES: ', res.data.ParsedResults[0].TextOverlay.Lines);
+      lines.forEach((line) => {
+        let ocrResponseModel = new OcrLineResponseModel();
+
+        //console.log('LINE: ', line);
+        ocrResponseModel.lineText = line.LineText;
+        line.Words.forEach((word) => {
+          ocrResponseModel.words.push(word);
+        });
+        responseLines.push(ocrResponseModel);
+      });
+      console.log('OBTAINED BILL: ', responseLines);
+    });
+  }
+
+  scanToWebPageAndUpload() {
+    var scanRequest = {
+      "use_asprise_dialog": true, // Whether to use Asprise Scanning Dialog
+      "show_scanner_ui": false, // Whether scanner UI should be shown
+      "twain_cap_setting": { // Optional scanning settings
+        "ICAP_PIXELTYPE": "TWPT_RGB" // Color
+      },
+      "output_settings": [{
+        "type": "return-base64",
+        "format": "jpg"
+      }]
+    };
+
+    var scanRequest2 = {
+      "twain_cap_setting": {
+        "ICAP_PIXELTYPE": "TWPT_RGB", // Color
+        "ICAP_SUPPORTEDSIZES": "TWSS_USLETTER" // Paper size: TWSS_USLETTER, TWSS_A4, ...
+      },
+      "output_settings": [
+        { "type": "return-base64", "format": "jpg" }, // return images to web page
+        {
+          "type": "upload", "format": "pdf", // upload as PDF
+          "upload_target": {
+            "url": "https://asprise.com/scan/applet/upload.php?action=dump"
+          }
+        }
+      ]
+    };
+
+    scanner.scan(function displayImagesOnPage(successful, mesg, response) {
+      if (!successful) { // On error
+        console.error('Failed: ' + mesg);
+        return;
+      }
+
+      if (successful && mesg != null && mesg.toLowerCase().indexOf('user cancel') >= 0) { // User canceled.
+        console.info('User canceled');
+        return;
+      }
+
+      var scannedImages = scanner.getScannedImage(response, true, false); // returns an array of ScannedImage
+      for (var i = 0; (scannedImages instanceof Array) && i < scannedImages.length; i++) {
+        var scannedImage = scannedImages[i];
+        processScannedImage(scannedImage);
+      }
+    }, scanRequest);
+
+    /** Processes the scan result */
+    /* function displayImagesOnPage(successful, mesg, response) {
+      if (!successful) { // On error
+        console.error('Failed: ' + mesg);
+        return;
+      }
+
+      if (successful && mesg != null && mesg.toLowerCase().indexOf('user cancel') >= 0) { // User canceled.
+        console.info('User canceled');
+        return;
+      }
+
+      var scannedImages = scanner.getScannedImage(response, true, false); // returns an array of ScannedImage
+      for (var i = 0; (scannedImages instanceof Array) && i < scannedImages.length; i++) {
+        var scannedImage = scannedImages[i];
+        processScannedImage(scannedImage);
+      }
+    } */
+
+    /** Processes a ScannedImage */
+    function processScannedImage(scannedImage) {
+      let imagesScanned = [];
+      imagesScanned.push(scannedImage);
+      let img = document.createElement('img');
+      img.name = 'img';
+      img.src = scannedImage.src;
+      document.getElementById('body').appendChild(img);
+      //down.innerHTML = "Image Element Added.";
+
+      /*var elementImg = createDomElementFromModel({
+        'name': 'img',
+        'attributes': {
+          'class': 'scanned',
+          'src': scannedImage.src
+        }
+      });
+      document.getElementById('images').appendChild(elementImg);*/
     }
   }
 
@@ -86,7 +261,7 @@ class App extends Component {
             })}
           </div>
 
-          <button className="button" onClick={this.generateText}>Generate</button>
+          <button className="button" onClick={this.generatetextFromBase64Image}>Generate</button>
         </section>
 
         { /* Results */}
@@ -112,6 +287,8 @@ class App extends Component {
             )
           })}
         </section>
+        <div onClick={this.scanToWebPageAndUpload}>Scan image</div>
+        <div id="images"></div>
       </div>
     )
   }
---
+++ demo_tesseract/src/OcrLineResponseModel.js
@@ -0,0 +1,48 @@
+export class OcrLineResponseModel {
+
+    constructor() {
+        this.lineText = '';
+        //this.words = [{ WordText: '' }];
+        this.words = [];
+    }
+
+    /*"ParsedResults": [
+        {
+            "TextOverlay": {
+                "Lines": [
+                    {
+                        "LineText": "Tel.: 0341 - 4261459",
+                        "Words": [
+                            {
+                                "WordText": "Tel.:",
+                                "Left": 71,
+                                "Top": 315,
+                                "Height": 27,
+                                "Width": 63
+                            },
+                            {
+                                "WordText": "0341",
+                                "Left": 150,
+                                "Top": 315,
+                                "Height": 27,
+                                "Width": 70
+                            },
+                            {
+                                "WordText": "-",
+                                "Left": 234,
+                                "Top": 329,
+                                "Height": 4,
+                                "Width": 11
+                            },
+                            {
+                                "WordText": "4261459",
+                                "Left": 256,
+                                "Top": 315,
+                                "Height": 27,
+                                "Width": 128
+                            }
+                        ],
+                        "MaxHeight": 27,
+                        "MinTop": 315
+                    },*/
+}
